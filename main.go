62d4ef217060a1cd27ede3e839fb0f7a1ccd00b1
jay <dj.min43@gmail.com>
2022-06-12 16:11:13 +0900

    crawl pokemon korea

--- main.go
+++ main.go
@@ -2,31 +2,20 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("https://ko.wikipedia.org/wiki/%ED%8F%AC%EC%BC%93%EB%AA%AC_%EB%AA%A9%EB%A1%9D"),
-	)
+	c := colly.NewCollector()
 
-	// On every element which has href attribute call callback
+	// Find and visit all links
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
+		e.Request.Visit(e.Attr("href"))
 	})
 
-	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		fmt.Println("Visiting", r.URL)
 	})
 
-	c.Visit("https://ko.wikipedia.org/wiki/%ED%8F%AC%EC%BC%93%EB%AA%AC_%EB%AA%A9%EB%A1%9D")
+	c.Visit("https://www.pokemonkorea.co.kr/pokedex")
 }
